ddd59e4e703d21d6f53eb14c64ee3d1cf42b5a6d
shangzw <sahngzw12@163.com>
2016-03-16 17:59:07 +0800

    -

--- LEC8/addr.py
+++ LEC8/addr.py
@@ -1,7 +1,6 @@
 def read_data():
 	raw_data = input("Please input v_addr: ")
 	data = raw_data
-	
 	print(data)
 	return data
 
@@ -35,26 +34,25 @@ def addr_convert(v_addr, mem, disk):
 		return 
 	pte_base_addr = pfn[1:8]
 	#print("pte_addr: "+ str(pte_addr))
-	print(str(pte_base_addr))
-	
+	#print(str(pte_base_addr))
 	ptn = mem[int(pte_base_addr,2)][pte_id]
 	ptn = format(int(ptn, 16), '08b')
 	valid_ptn = ptn[0:1]
 	print ("pte_index: "+ str(hex(pte_id)))
 	print ("pte_content: "+ str(ptn)+ "  valid: "+ str(valid_ptn)+"  ptn: "+str(hex(int(ptn[1:8], 2))))
-	if(not valid_ptn):
+	if(not eval(valid_ptn)):
 		print("Fault! Search in disk!")
 		value = disk[int(ptn[1:8], 2)][offset]
-		print ("--> value: "+ str(value))
+		print ("--> value: 0x"+ str(value))
 		return
 	value = mem[int(str(ptn[1:8]), 2)][offset]
-	print ("--> value: "+ str(value))
+	print ("--> value: 0x"+ str(value))
 	
 def my_func():
 	mem = read_file('table')
-	print (mem)
+	#print (mem)
 	disk = read_file('disk')
-	print (disk)
+	#print (disk)
 	v_addr = read_data()
 	addr_convert(v_addr, mem, disk)
 
